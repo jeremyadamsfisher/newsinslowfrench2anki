a2e185d33c63aca0c1d6143ab238442cc1bd9878
Jeremy Fisher <jeremyadamsfisher@gmail.com>
2022-12-10 18:19:17 -0800

    Open in anki to import cards

--- newsinslowfrenchdaemon/daemon.py
+++ newsinslowfrenchdaemon/daemon.py
@@ -1,8 +1,12 @@
 import csv
+import subprocess
 import time
 from tempfile import NamedTemporaryFile
 from typing import List, Tuple
 
+import genanki
+from anki.collection import Collection
+from anki.importing import TextImporter
 from playwright.sync_api import sync_playwright
 
 from newsinslowfrenchdaemon.config import settings
@@ -18,10 +22,10 @@ ANKI_EDIT_URL = "https://ankiuser.net/edit/"
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=settings.web_scraping.headless)
         page = browser.new_page()
         vocab = get_most_recent_vocab(page)
-        add_vocab_to_anki(page, vocab)
+        add_vocab_to_anki(vocab)
         browser.close()
 
 
@@ -35,8 +39,10 @@ def get_most_recent_vocab(page) -> VocabList:
     page.get_by_role("button", name="Login").click()
 
     # Find the most recent episode
+    time.sleep(5)  # avoid concurrent navigation
     page.goto(NISF_NEWSPAGE_URL)
     most_recent_episode_link = page.locator('css=.episode a[tabindex="0"]')
+    time.sleep(5)  # avoid concurrent navigation
     page.goto(NISF_BASE_URL + most_recent_episode_link.get_attribute("href"))
 
     # Download the vocab CSV
@@ -48,7 +54,7 @@ def get_most_recent_vocab(page) -> VocabList:
 
     # Extract the vocab from the annoyingly formatted CSV
     vocab = []
-    with NamedTemporaryFile(suffix=".csv") as t:
+    with NamedTemporaryFile(suffix=".csv", mode="wt+") as t:
         download.save_as(t.name)
         t.seek(0)
         reader = csv.reader(t)
@@ -61,22 +67,25 @@ def get_most_recent_vocab(page) -> VocabList:
     return vocab
 
 
-def add_vocab_to_anki(page, vocab: VocabList):
-
-    # Log in to Anki
-    page.goto(ANKI_LOGIN_URL)
-    page.get_by_label("Email").fill(settings.anki.username)
-    page.get_by_label("Password").fill(settings.anki.password)
-    page.get_by_role("button", name="Log in").click()
-
-    # Add each as a card with one language on either side
-    page.goto(ANKI_EDIT_URL)
+def add_vocab_to_anki(vocab: VocabList):
+    col = Collection(settings.anki.collection_fp)
+    try:
+        deck_id = col.decks.id(settings.anki.deck_name)
+    finally:
+        col.close()
+    deck = genanki.Deck(deck_id, settings.anki.deck_name)
+    print(f"Adding {len(vocab)} cards...")
     for french, english in vocab:
-        page.get_by_label("Type").select_option("1")  # 1 = Basic (and reversed card)
-        page.locator("#f0").fill(french)  # Front
-        page.locator("#f1").fill(english)  # Back
-        page.get_by_role("button", name="Save").click()
-        time.sleep(0.5)
+        note = genanki.Note(
+            model=genanki.BASIC_AND_REVERSED_CARD_MODEL,
+            fields=[french, english],
+        )
+        deck.add_note(note)
+    with NamedTemporaryFile(suffix=".apkg") as t:
+        genanki.Package(deck).write_to_file(t.name)
+        t.seek(0)
+        subprocess.check_call(["open", t.name])
+        time.sleep(15)  # wait for anki to import, then quit
 
 
 if __name__ == "__main__":
